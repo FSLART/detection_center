2f7ab9e87d377bfad5e997ec309e507c06134bc3
Ian Rosales <ianmorais60@gmail.com>
2026-04-27 09:30:23 +0100

    Hardcode engine path to the detect. constructor

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,7 @@
 int main(int argc, char* argv[]) {
     rclcpp::init(argc, argv);
 
-    if (argc < 2) {
-        std::cerr << "Usage: detection_center <path_to_engine>" << std::endl;
-        return 1;
-    }
-
-    DetectionCenter detector(argv[1]);
+    DetectionCenter detector("path to engine file");
     auto zed_node = std::make_shared<ZedCenter>(rclcpp::NodeOptions(), detector);
 
     rclcpp::spin(zed_node);
